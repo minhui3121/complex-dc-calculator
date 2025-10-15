e4157f037013d4c6b1e08ff388182dbcb1433ab3
MINHUI <minhui.roh@mail.mcgill.ca>
2025-10-15 16:34:30 -0400

    Test5-ADD_REAL1 passing

--- cdc.py
+++ cdc.py
@@ -1,13 +1,18 @@
 import sys
 from collections import deque
 
+token = ["push", "pop", "add", "sub", "mul", "div", "del"]
+
 def main():
     stack = deque()
     args = sys.argv[1:]
     result = ""
     for i in range(len(args)):
         if args[i] == "push":
-            number = args[i+1:len(args)-1]
+            j = 1
+            while args[i + j] not in token:
+                j += 1
+            number = args[i+1:i+j]
             number = "".join(number)
             stack.append(number)
         elif args[i] == "pop":
@@ -16,6 +21,10 @@ def main():
             except IndexError:
                 print("Error: stack underflow")
                 return
+        elif args[i] == "add":
+            total = sum(int(x) for x in stack)
+            stack.clear
+            stack.append(str(total))
     if "j" not in result:
         result = result + " + j0"
     else:
