e8edffb02fc52c481e87e023bed837ef55d81808
MINHUI <minhui.roh@mail.mcgill.ca>
2025-10-15 15:22:31 -0400

    Test2-PUSH_CPLX1 failing

--- test_cdc.py
+++ test_cdc.py
@@ -8,3 +8,11 @@ def test_push_real1():
         text = True
     )
     assert result.stdout.strip("\n") == "5 + j0"
+
+def test_push_cplx1():
+    result = subprocess.run(
+        ["python3", "cdc.py", "push", "-2.5-j0.25", "pop"],
+        capture_output=True,
+        text=True
+    )
+    assert result.stdout.strip("\n") == "-2.5 - j0.25"
