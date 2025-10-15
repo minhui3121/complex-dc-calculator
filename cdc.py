b98cf67f3ef6947d496b395e90fb0fc0fc433901
MINHUI <minhui.roh@mail.mcgill.ca>
2025-10-15 15:41:25 -0400

    Test3-PUSH_CPLX2 passing

--- cdc.py
+++ cdc.py
@@ -1,11 +1,12 @@
 import sys
 
 def main():
-    number = sys.argv[2]
+    number = sys.argv[2:len(sys.argv)-1] # Exclude push and pop
+    number = "".join(number)
     if "j" not in number:
         number = number + " + j0"
     else:
-        number = number.replace("-", " -").replace("j", " j")
+        number = number.replace("-", " -").replace("+", " +").replace("j", " j")
         number = number.strip()
     print(number)
     
