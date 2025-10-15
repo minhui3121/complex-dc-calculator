0fdd47e59d5a10a6cf88de80aa5ac6e9b8cbf43f
MINHUI <minhui.roh@mail.mcgill.ca>
2025-10-15 15:26:50 -0400

    Test2-PUSH_CPLX1 passing

--- cdc.py
+++ cdc.py
@@ -1,6 +1,14 @@
-def main():
-    print("5 + j0")
+import sys
 
+def main():
+    number = sys.argv[2]
+    if "j" not in number:
+        number = number + " + j0"
+    else:
+        number = number.replace("-", " -").replace("j", " j")
+        number = number.strip()
+    print(number)
+    
 
 if __name__ == "__main__":
     main()
